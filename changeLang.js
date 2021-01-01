1c4168205f863910632ccddd6915f63ef9f88a6f
Mia Coupland <42342538+miacoupland@users.noreply.github.com>
2021-01-01 19:38:49 +0000

    Add files via upload

---
+++ changeLang.js
@@ -0,0 +1,12 @@
+function toggleLanguage(language) {
+    var x = document.getElementsByClassName('en-UK');
+    x.style.display = "none";
+}
+
+/*
+var dict = {
+    "about": "pri mi",
+    "my work": "mia laboro",
+    "blog": "blogo",
+    "contact": "kontacto"
+}*/
