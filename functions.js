bfb36547c1a99ab64f4e92700d33f600a49698c1
Mia Coupland <42342538+miacoupland@users.noreply.github.com>
2021-01-02 18:59:38 +0000

    Add files via upload

--- functions.js
+++ functions.js
@@ -6,13 +6,3 @@ function showHamburger() {
         x.style.display = "block";
     }
 }
-
-function mail(form) {
-    var name = form.name.value;
-    var subject = form.subject.value;
-    var country = form.country.value;
-    var message = form.message.value;
-
-    var str = "mailto:miacoupland@mail.com?subject=" + subject + "&body=" + name + "\n" + country + "\n" + message;
-    document.getElementById('send').href = str;
-}
