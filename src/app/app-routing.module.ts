488245b21c5bd18bd0b2724b4481830adff756ea
Mia Coupland <42342538+miacoupland@users.noreply.github.com>
2022-07-09 20:47:18 +0100

    Configure header and routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,54 +3,52 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './presentation/article/article.component';
 import { BioComponent } from './presentation/bio/bio.component';
+import { PageNotFoundComponent } from './presentation/common/page-not-found/page-not-found.component';
 import { ContactComponent } from './presentation/contact/contact.component';
 import { ProjectsComponent } from './presentation/projects/projects.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: AppComponent,
-    data: {
-      productName: '',
-      pageTitle: '',
-    },
-    children: [
-      {
-        path: '',
-        data: { hideNavItem: true },
-        redirectTo: '',
-        pathMatch: 'full',
-      },
-      {
-        path: 'About',
-        component: BioComponent,
-        data: {
-          pageTitle: 'About',
-        },
-      },
-      {
-        path: 'Blog',
-        component: ArticleComponent,
-        data: {
-          pageTitle: 'Blog',
-        },
-      },
-      {
-        path: 'Contact',
-        component: ContactComponent,
-        data: {
-          pageTitle: 'Contact',
-        },
-      },
-      {
-        path: 'Projects',
-        component: ProjectsComponent,
-        data: {
-          pageTitle: 'Projects',
-        },
-      },
-    ],
+    path: 'Home',
+    component: AppComponent
   },
+  {
+    path: 'About',
+    component: BioComponent,
+    data: {
+      pageTitle: 'About',
+    },
+  },
+  {
+    path: 'Blog',
+    component: ArticleComponent,
+    data: {
+      pageTitle: 'Blog',
+    },
+  },
+  {
+    path: 'Contact',
+    component: ContactComponent,
+    data: {
+      pageTitle: 'Contact',
+    },
+  },
+  {
+    path: 'Projects',
+    component: ProjectsComponent,
+    data: {
+      pageTitle: 'Projects',
+    },
+  },
+  {
+    path: '',
+    redirectTo: 'Home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
