3e621b2222cb49bb73f70418678303bc704b92c3
Mia Coupland <42342538+miacoupland@users.noreply.github.com>
2022-08-29 19:29:59 +0100

    projects compatible with config file

--- src/app/presentation/projects/project-tile/project-tile/project-tile.component.ts
+++ src/app/presentation/projects/project-tile/project-tile/project-tile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Project } from 'src/app/presentation/common/entities/project-entity';
 
 @Component({
   selector: 'app-project-tile',
@@ -8,17 +9,17 @@ import { Input } from '@angular/core';
 })
 export class ProjectTileComponent implements OnInit {
   @Input() public tileData: any;
-  public key: string;
-  public name: string;
-  public imageSrc: string;
-  public githubLink: string;
+  public key: string = '';
+  public name: string = '';
+  public imageSrc: string = '';
+  public githubLink: string = '';
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
     this.key = this.tileData.key;
     this.name = this.tileData.name;
     this.imageSrc = this.tileData.imageSrc;
     this.githubLink = this.tileData.githubLink;
   }
-
-  ngOnInit(): void {}
 }
