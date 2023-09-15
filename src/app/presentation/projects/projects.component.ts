ad636de14662c88734d29f6af0a1d1b0c3194e6e
Mia Coupland <miacoupland@btinternet.com>
2023-09-15 20:28:03 +0100

    Add title service

--- src/app/presentation/projects/projects.component.ts
+++ src/app/presentation/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Project } from '../common/entities/project-entity';
 import config from './../../../assets/config.json';
+import { TitleService } from '../services/title.service';
 
 @Component({
   selector: 'app-projects',
@@ -11,11 +12,13 @@ import config from './../../../assets/config.json';
 export class ProjectsComponent implements OnInit {
   public tileData: Observable<Project[]> = of(Object.values(config.projects));
 
-  constructor() {
+  constructor(private titleService: TitleService) {
     this.tileData.subscribe((value) => {
       return [value[0], value[1]];
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.titleService.setTitle('common.projects');
+  }
 }
