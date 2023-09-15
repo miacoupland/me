48cfbd0d49ae42aea959756707add26e5094111c
Mia Coupland <miacoupland@btinternet.com>
2023-09-15 20:34:33 +0100

    Add landing component

---
+++ src/app/presentation/common/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { TitleService } from '../../services/title.service';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+})
+export class LandingComponent implements OnInit {
+
+  constructor(private titleService: TitleService) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('common.home');
+  }
+}
