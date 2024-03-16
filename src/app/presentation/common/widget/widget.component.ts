06542d82c6fbf5db6018e2302f1c2c8a1be259b5
Mia Coupland <miacoupland@btinternet.com>
2024-03-16 17:14:44 +0000

    Style widgets like windows

--- src/app/presentation/common/widget/widget.component.ts
+++ src/app/presentation/common/widget/widget.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'widget',
   templateUrl: './widget.component.html',
-  styleUrls: [],
+  styleUrls: ['./widget.component.scss'],
 })
 export class WidgetComponent {
   @Input() public title!: string;
