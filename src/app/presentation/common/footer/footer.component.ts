4fdce4bed8851f9a885b2ddcb4cf4d26963d3fbd
Mia Coupland <miacoupland@btinternet.com>
2024-03-23 19:14:19 +0000

    Use footer as navigation

--- src/app/presentation/common/footer/footer.component.ts
+++ src/app/presentation/common/footer/footer.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: [],
 })
 export class FooterComponent implements OnInit {
+  public menu: boolean = false;
+  public dateTime: Date;
 
-  constructor() { }
+  @Output() public projectsChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() public bioChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() public contactChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+
+  constructor() {
+    this.dateTime = new Date();
   }
 
+  ngOnInit(): void {}
+
+  public toggleMenu(): void {
+    this.menu = !this.menu;
+  }
+
+  public showProjects(): void {
+    this.projectsChange.next(true);
+  }
+
+  public showBio(): void {
+    this.bioChange.next(true);
+  }
+
+  public showContact(): void {
+    this.contactChange.next(true);
+  }
 }
